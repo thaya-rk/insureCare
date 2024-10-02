54d0c17c3a2320f0d90660b159232ae7eb13453e
thayark <rkt12357@gmail.com>
2024-10-02 21:03:51 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+    
